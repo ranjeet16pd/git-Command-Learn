c94f9a2a21169a8e03f687c24b4b62b327055740
ranjeetprasad <ranjeet.prasad@paytm.com>
2025-06-06 09:47:56 +0530

    added fucntion for addtion of two numebr

--- learn.java
+++ learn.java
@@ -1,5 +1,12 @@
 public class learn{
     public static void main(String... args) {
         System.out.print("Accha Bhai Theek h ");
+
+        System.out.println(addTwoNumber(3,44));
+
+    }
+
+    public static int addTwoNumber(int a,int b){
+        return a+b;
     }
 }
